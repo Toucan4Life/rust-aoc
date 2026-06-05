6d49f7f853f918286bdd2a38bb13841de5ece03a
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2026-06-05 14:50:45 +0000

    day 3 part 2

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,43 +1,80 @@
 fn find_joltage_part1(batteries: String) -> u32 {
     let mut best = 0;
-    let batteries_int:Vec<u32>=batteries.chars().map(|x| x.to_digit(10).unwrap()).collect();
-    for i in 0..batteries_int.iter().count()-1{
-        for j in i+1..batteries_int.iter().count(){
-            let candidate = batteries_int[i]*10+batteries_int[j];
-            if candidate>best{
+    let batteries_int: Vec<u32> = batteries.chars().map(|x| x.to_digit(10).unwrap()).collect();
+    for i in 0..batteries_int.len() - 1 {
+        for j in i + 1..batteries_int.len() {
+            let candidate = batteries_int[i] * 10 + batteries_int[j];
+            if candidate > best {
                 best = candidate
             }
         }
     }
-    best.try_into().unwrap()
+    best
 }
-fn find_ordered_permutation(batteries: String, k: i32){
-    if k==1 {
-        
+
+fn find_max_subnumber(batteries: String, k: usize) -> Vec<char> {
+    let mut digit = Vec::<char>::new();
+    for (i, ch) in batteries.char_indices() {
+        while batteries.len() - i > k - digit.len()
+            && digit
+                .pop_if(|last_char| ch.to_digit(10) > last_char.to_digit(10))
+                .is_some()
+        {}
+
+        if digit.len() < k {
+            digit.push(ch);
+        }
     }
+
+    digit
 }
+
+// 17493
+// 173685428989126
 fn main() {
     let input = include_str!("./input.txt");
-    let part1 :u32= input.lines().map(|line| find_joltage_part1(line.to_string())).sum();
+    let part1: u32 = input
+        .lines()
+        .map(|line| find_joltage_part1(line.to_string()))
+        .sum();
     println!("{part1}");
+    let part2: i64 = input
+        .lines()
+        .map(|line| {
+            find_max_subnumber(line.to_string(), 12)
+                .iter()
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap()
+        })
+        .sum();
+    println!("{part2}");
 }
 
 #[test]
-fn part1(){
+fn part1() {
     let input = "987654321111111";
     let input = "811111111111119";
     let input = "234234234234278";
     let input = "818181911112111";
     let jolt = find_joltage_part1(input.to_string());
-    assert_eq!(jolt, 89)
+    assert_eq!(jolt, 92)
 }
 
 #[test]
-fn part2(){
+fn part2() {
     let input = "987654321111111";
     let input = "811111111111119";
-    let input = "234234234234278";
-    let input = "818181911112111";
-    let jolt = find_joltage_part1(input.to_string());
-    assert_eq!(jolt, 89)
+    // let input = "234234234234278";
+    // let input = "818181911112111";
+    // let input = "34234234234278";
+    // let input = "12989191111211";
+    // let input = "165878981214187";
+    let jolt = find_max_subnumber(input.to_string(), 12)
+        .iter()
+        .collect::<String>()
+        .parse::<i64>()
+        .unwrap();
+    // assert_eq!(jolt, 878981214187)
+    assert_eq!(jolt, 811111111119)
 }
