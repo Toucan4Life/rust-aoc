2812995229b33a406fe294a6e46381344fe20424
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2026-06-12 12:58:29 +0000

    more idiomatic version

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,42 +1,42 @@
 fn main() {
-    let input = include_str!("./input.txt");
+    let input = include_str!("./input.txt").lines().map(|l| l.chars().collect()).collect();
     let paper_number = part1(input);
-    println!("Part 1: {}", paper_number)
+    println!("Part 1: {paper_number}");
 }
 
-fn part1(input: &str) -> i32 {
-    let lines: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
-
-    lines.iter()
+fn part1(input: Vec<Vec<char>>) -> usize {
+    input
+        .iter()
         .enumerate()
         .map(|(i, row)| {
-            row.iter().enumerate()
-                .filter(|&(j, &c)| c == '@' && get_neighbor_count(&lines, i, j) < 5)
+            row.iter()
+                .enumerate()
+                .filter(|&(j, &c)| c == '@' && get_neighbor_count(&input, i, j) < 4)
                 .count()
         })
-        .sum::<usize>() as i32
+        .sum::<usize>()
 }
 
-fn get_neighbor_count(lines: &Vec<Vec<char>>, i: usize, j: usize) -> usize {
-    [-1, 0, 1].iter()
-        .flat_map(|x_offset| {
-            [-1, 0, 1].iter().filter_map(|y_offset| {
-                if let Some(line) = i.checked_add_signed(*y_offset).and_then(|i_index| lines.get(i_index))
-                    && let Some('@') = j.checked_add_signed(*x_offset).and_then(|j_index| line.get(j_index))
-                {
-                    Some(())
-                } else {
-                    None
-                }
-            })
-        })
-        .count()
+fn get_neighbor_count(lines: &[Vec<char>], i: usize, j: usize) -> usize {
+    [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1),
+    ]
+    .iter()
+    .filter_map(|(di, dj)| {
+        let ni = i.checked_add_signed(*di)?;
+        let nj = j.checked_add_signed(*dj)?;
+        Some(lines.get(ni)?.get(nj)?)
+    })
+    .filter(|&&c| c == '@')
+    .count()
 }
 
 #[test]
 fn test_part_1() {
     let input = "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@.";
 
-    let paper_number = part1(input);
+    let paper_number = part1(input.lines().map(|l| l.chars().collect()).collect());
     assert_eq!(13, paper_number)
 }
